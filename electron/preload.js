0f6fd0b2a3d6d39e465d10d8244c8dbc5d092811
AnishtayiN <massagetoanishtayin@gmail.com>
2026-08-05 06:37:54 +0330

    Update preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -3,4 +3,10 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('sonicwave', {
   selectFolder: () => ipcRenderer.invoke('select-folder'),
   listAudioFiles: (folderPath) => ipcRenderer.invoke('list-audio-files', folderPath),
+  renameFile: (oldPath, newPath) => ipcRenderer.invoke('file-rename', oldPath, newPath),
+  deleteFile: (filePath) => ipcRenderer.invoke('file-delete', filePath),
+  revealFile: (filePath) => ipcRenderer.invoke('file-reveal', filePath),
+  minimize: () => ipcRenderer.invoke('win-minimize'),
+  maximize: () => ipcRenderer.invoke('win-maximize'),
+  closeWindow: () => ipcRenderer.invoke('win-close'),
 });
