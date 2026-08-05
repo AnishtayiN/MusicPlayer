c1606c93820c67c5ff3df16ba2a95981a54f8502
AnishtayiN <massagetoanishtayin@gmail.com>
2026-08-05 06:37:26 +0330

    Update web_bridge_stub.dart

--- lib/utils/web_bridge_stub.dart
+++ lib/utils/web_bridge_stub.dart
@@ -1,4 +1,4 @@
-/// نسخه اندروید / پلتفرم‌های غیر وب
+/// نسخه اندروید / غیر وب
 bool get isElectronBridgeAvailable => false;
 
 Future<String?> selectFolderNative() async => null;
@@ -6,3 +6,11 @@ Future<String?> selectFolderNative() async => null;
 Future<List<String>> listAudioFilesNative(String folderPath) async => const [];
 
 String localFileUrl(String filePath) => filePath;
+
+void setTitleBarColor(String bg, String fg) {}
+
+Future<bool> renameFileNative(String oldPath, String newPath) async => false;
+
+Future<bool> deleteFileNative(String path) async => false;
+
+void revealFileNative(String path) {}
