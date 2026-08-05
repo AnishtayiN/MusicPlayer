4cf35eccbcde3d9c5979f8b5ed927ca767c1ae65
AnishtayiN <massagetoanishtayin@gmail.com>
2026-08-05 07:33:02 +0330

    Update web_bridge_stub.dart

--- lib/utils/web_bridge_stub.dart
+++ lib/utils/web_bridge_stub.dart
@@ -1,16 +1,14 @@
-/// نسخه اندروید / غیر وب
 bool get isElectronBridgeAvailable => false;
 
 Future<String?> selectFolderNative() async => null;
-
-Future<List<String>> listAudioFilesNative(String folderPath) async => const [];
-
-String localFileUrl(String filePath) => filePath;
-
+Future<List<String>> listAudioFilesNative(String p) async => const [];
+String localFileUrl(String p) => p;
 void setTitleBarColor(String bg, String fg) {}
-
-Future<bool> renameFileNative(String oldPath, String newPath) async => false;
-
-Future<bool> deleteFileNative(String path) async => false;
-
-void revealFileNative(String path) {}
+Future<bool> renameFileNative(String a, String b) async => false;
+Future<bool> deleteFileNative(String p) async => false;
+void revealFileNative(String p) {}
+Future<Map<String, dynamic>?> getMetadataNative(String p) async => null;
+Future<String?> readFileTextNative(String p) async => null;
+Future<String?> getMusicDirNative() async => null;
+void onFilesDropped(void Function(List<String>) handler) {}
+void onMediaKey(void Function(String) handler) {}
