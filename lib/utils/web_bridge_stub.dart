1aa35945c8794dbdf2f60a8cee9243636dc50bb4
AnishtayiN <massagetoanishtayin@gmail.com>
2026-08-05 05:27:43 +0330

    Create web_bridge_stub.dart

---
+++ lib/utils/web_bridge_stub.dart
@@ -0,0 +1,8 @@
+/// نسخه اندروید / پلتفرم‌های غیر وب
+bool get isElectronBridgeAvailable => false;
+
+Future<String?> selectFolderNative() async => null;
+
+Future<List<String>> listAudioFilesNative(String folderPath) async => const [];
+
+String localFileUrl(String filePath) => filePath;
